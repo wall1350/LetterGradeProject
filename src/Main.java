1b4415e68806299e8f99c38e65fae2f7300f0b15
William-ubuntu <wall1350@gmail.com>
2020-12-28 21:02:46 +0800

    add main

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        LetterGrade letterGrade = new LetterGrade();
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade =  letterGrade.getGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
